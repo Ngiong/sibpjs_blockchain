0cca4c5672d3901c2143d0084b31f4c307a1461b
Robert Herlim <robert.herlim@airyrooms.com>
2019-10-26 17:59:49 +0700

    Navigation Bar

--- client/src/app/routes.js
+++ client/src/app/routes.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, Link } from 'react-router-dom'
+import { Redirect, Route, Link } from 'react-router-dom'
 
 import AccountPage from '../containers/AccountPage'
 import DocumentPage from '../containers/DocumentPage'
@@ -14,25 +14,12 @@ const getRoutes = drizzleProps => {
   const routes = [
     <Route key='account-page' exact path='/account' render={_AccountPage(drizzleProps)} />,
     <Route key='document-page' exact path='/document' render={_DocumentPage(drizzleProps)} />,
-    <Route key='request-page' exact path='/request' render={_RequestPage(drizzleProps)} />,
+    <Route key='access-request-page' exact path='/access' render={_RequestPage(drizzleProps)} />,
+    <Redirect key='default-page' from="/" to="/document" />,
   ]
   return routes
 }
 
-class Menu extends React.Component {
-  render = () => {
-    return <div style={{ textAlign: 'center' }}>
-      <h1>Menu</h1>
-      <ul>
-        <li><Link to="/account">Account Page</Link></li>
-        <li><Link to="/document">Document Page</Link></li>
-        <li><Link to="/request">Request Page</Link></li>
-      </ul>
-    </div>
-  }
-}
-
 export {
-  Menu,
   getRoutes,
 }
