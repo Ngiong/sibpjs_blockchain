7c2c2f7337d6794c86592b04617dd415577b378a
Devin Ryan Riota <devin.riota@airyrooms.com>
2019-10-27 14:58:36 +0700

    WIP

--- contracts/Document.sol
+++ contracts/Document.sol
@@ -1,17 +1,30 @@
 pragma solidity ^0.5.0;
+pragma experimental ABIEncoderV2;
 
 contract Document {
   uint256 public totalDocument;
-  mapping (uint256 => string) public document;
-  mapping (uint256 => address) public documentOwner;
-  mapping (address => uint256[]) public ownedDocumentList;
-  mapping (address => uint256[]) public authorizedDocumentList;
+  mapping (uint256 => string) public document; //document id to document data
+  mapping (uint256 => address) public documentOwner; //document id to user (owner) address
+  mapping (address => uint256[]) public ownedDocumentList; //address to list of document ids
+  mapping (address => uint256[]) public authorizedDocumentList; //address to list of authorized documents
+  
+  mapping(address => DocumentData[]) public ownedDocumentOfAddress;
+  mapping(address => DocumentData[]) public authorizedDocumentOfAddress;
 
+  struct DocumentData {
+    uint256 id;
+    string documentType;
+    string data;
+    string signature;
+  }
+
+  // TODO: if unused, delete this
   struct OwnedDocumentData {
     uint256 id;
     string data; // TODO: nambah digital signature
   }
 
+  // TODO: if unused, delete this
   struct AuthorizedDocumentData {
     uint256 id;
     address owner;
@@ -38,5 +51,22 @@ contract Document {
 
   function getAuthorizedDocumentList(address _account) public view returns (uint256[] memory) {
     return authorizedDocumentList[_account];
+  }
+
+  function createDocumentNew(address _owner, string memory _type, string memory _data) public {
+    createDocument(_owner, _data);
+
+    //assume totalDocument is incremented in createDocument() method
+    DocumentData[] storage currentDocuments = ownedDocumentOfAddress[_owner];
+    DocumentData memory newDocument = DocumentData(totalDocument, _type, _data, "signature");
+    currentDocuments.push(newDocument);
+  }
+
+  function getOwnedDocumentOf(address _owner) public view returns (DocumentData[] memory) {
+    return ownedDocumentOfAddress[_owner];
+  }
+
+  function getAuthorizedDocumentOf(address _owner) public view returns (DocumentData[] memory) {
+    return authorizedDocumentOfAddress[_owner];
   }
 }
