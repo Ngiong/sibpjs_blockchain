b070ef90b0df899947a2aebd24b0fbfd48d03c01
Devin Ryan Riota <devin.riota@airyrooms.com>
2019-10-28 20:24:16 +0700

    update Document.sol

--- client/src/containers/DocumentPage/DocumentPage.js
+++ client/src/containers/DocumentPage/DocumentPage.js
@@ -87,9 +87,10 @@ class DocumentPage extends ReactDrizzleComponent {
     </div>
 
     return <div>
-      {this.props.mode === 'CREATE' && createSection}
+      {createSection}{listSection}{viewSection}
+      {/* {this.props.mode === 'CREATE' && createSection}
       {this.props.mode === 'LIST' && listSection}
-      {this.props.mode === 'VIEW' && viewSection}
+      {this.props.mode === 'VIEW' && viewSection} */}
     </div>
   }
 
--- contracts/Document.sol
+++ contracts/Document.sol
@@ -2,12 +2,14 @@ pragma solidity ^0.5.0;
 
 contract Document {
   uint256 public totalDocument;
-  mapping (uint256 => string) public document; //document id to document data
-  mapping (uint256 => address) public documentOwner; //document id to user (owner) address
+  uint256 public totalAuthorizedDocument;
+  // mapping (uint256 => string) public document; //document id to document data
+  // mapping (uint256 => address) public documentOwner; //document id to user (owner) address
   mapping (address => uint256[]) public ownedDocumentList; //address to list of document ids
   mapping (address => uint256[]) public authorizedDocumentList; //address to list of authorized documents
   
-  mapping(uint256 => DocumentData) public ownedDocumentOfId;
+  mapping(uint256 => DocumentData) public ownedDocumentData;
+  mapping(uint256 => AuthorizedDocumentData) public authorizedDocumentData;
   // mapping(string => uint256[]) public documentIdsOfType;
 
   struct DocumentData {
@@ -24,50 +26,50 @@ contract Document {
     string data;
   }
 
-  function createDocument(address _owner, string memory _data) public {
-    totalDocument++;
-    document[totalDocument] = _data;
-    documentOwner[totalDocument] = _owner;
-    ownedDocumentList[_owner].push(totalDocument);
-  }
-
-  function authorizeDocument(address _requester, address _granter, string memory _data) public {
-    totalDocument++;
-    document[totalDocument] = _data;
-    documentOwner[totalDocument] = _granter;
-    authorizedDocumentList[_requester].push(totalDocument);
-  }
-
-  function getOwnedDocumentList(address _account) public view returns (uint256[] memory) {
-    return ownedDocumentList[_account];
-  }
-
-  function getAuthorizedDocumentList(address _account) public view returns (uint256[] memory) {
-    return authorizedDocumentList[_account];
-  }
-
-  function createDocumentNew(address _owner, string memory _type, string memory _data) public {
-    createDocument(_owner, _data);
-
-    //assume totalDocument is incremented in createDocument() method
-    uint newId = totalDocument;
-    DocumentData memory newDocument = DocumentData(totalDocument, _type, _data, "signature");
-    ownedDocumentOfId[newId] = newDocument;
-
-    //insert mapping of document type to ids
-    // documentIdsOfType[_type].push(newId);
-  }
-
-  // function getDocumentByType(string memory _type) public view returns (uint256[] memory) {
-  //   return documentIdsOfType[_type];
+  // function createDocument(address _owner, string memory _data) public {
+  //   totalDocument++;
+  //   document[totalDocument] = _data;
+  //   documentOwner[totalDocument] = _owner;
+  //   ownedDocumentList[_owner].push(totalDocument);
   // }
 
-  function updateDocument(uint256 _id, string memory _data) public{
-    require(msg.sender == documentOwner[_id], "unauthorized");
-    document[_id] = _data;
-  }
+  // function authorizeDocument(address _requester, address _granter, string memory _data) public {
+  //   totalDocument++;
+  //   document[totalDocument] = _data;
+  //   documentOwner[totalDocument] = _granter;
+  //   authorizedDocumentList[_requester].push(totalDocument);
+  // }
 
-  function showSender() public view returns (address) {
-    return (msg.sender);
-  }
+  // function getOwnedDocumentList(address _account) public view returns (uint256[] memory) {
+  //   return ownedDocumentList[_account];
+  // }
+
+  // function getAuthorizedDocumentList(address _account) public view returns (uint256[] memory) {
+  //   return authorizedDocumentList[_account];
+  // }
+
+  // function createDocumentNew(address _owner, string memory _type, string memory _data) public {
+  //   createDocument(_owner, _data);
+
+  //   //assume totalDocument is incremented in createDocument() method
+  //   uint newId = totalDocument;
+  //   DocumentData memory newDocument = DocumentData(totalDocument, _type, _data, "signature");
+  //   ownedDocumentData[newId] = newDocument;
+
+  //   //insert mapping of document type to ids
+  //   // documentIdsOfType[_type].push(newId);
+  // }
+
+  // // function getDocumentByType(string memory _type) public view returns (uint256[] memory) {
+  // //   return documentIdsOfType[_type];
+  // // }
+
+  // function updateDocument(uint256 _id, string memory _data) public{
+  //   require(msg.sender == documentOwner[_id], "unauthorized");
+  //   document[_id] = _data;
+  // }
+
+  // function showSender() public view returns (address) {
+  //   return (msg.sender);
+  // }
 }
