cbbe0b8c03178cf321c6e210183dd8b17976fa81
Robert Herlim <robert.herlim@airyrooms.com>
2019-10-30 00:59:15 +0700

    Document Contract v2

--- contracts/Document.sol
+++ contracts/Document.sol
@@ -3,62 +3,72 @@ pragma solidity ^0.5.0;
 contract Document {
   uint256 public totalDocument;
   uint256 public totalAuthorizedDocument;
-  // mapping (uint256 => string) public document; //document id to document data
-  // mapping (uint256 => address) public documentOwner; //document id to user (owner) address
-  mapping (address => uint256[]) public ownedDocumentList; //address to list of document ids
-  mapping (address => uint256[]) public authorizedDocumentList; //address to list of authorized documents
+  mapping (address => uint256[]) public ownedDocumentList;
+  mapping (address => uint256[]) public authorizedDocumentList;
   
   mapping(uint256 => DocumentData) public ownedDocumentData;
   mapping(uint256 => AuthorizedDocumentData) public authorizedDocumentData;
+
+  // -- deprecated
+  // mapping (uint256 => string) public document; //document id to document data
+  // mapping (uint256 => address) public documentOwner; //document id to user (owner) address
   // mapping(string => uint256[]) public documentIdsOfType;
 
   struct DocumentData {
     uint256 id;
+    address issuer;
     string documentType;
     string data;
     string signature;
   }
 
-  // TODO: if unused, delete this
   struct AuthorizedDocumentData {
     uint256 id;
-    address owner;
-    string data;
+    address documentOwner;
+    string documentDataList;
   }
 
-  // function createDocument(address _owner, string memory _data) public {
-  //   totalDocument++;
-  //   document[totalDocument] = _data;
-  //   documentOwner[totalDocument] = _owner;
-  //   ownedDocumentList[_owner].push(totalDocument);
-  // }
+  function createDocument(
+    address _owner,
+    address _issuer,
+    string memory _type,
+    string memory _data,
+    string memory _signature) public {
 
-  // function authorizeDocument(address _requester, address _granter, string memory _data) public {
-  //   totalDocument++;
-  //   document[totalDocument] = _data;
-  //   documentOwner[totalDocument] = _granter;
-  //   authorizedDocumentList[_requester].push(totalDocument);
-  // }
+    totalDocument++;
+    ownedDocumentData[totalDocument] = DocumentData(totalDocument, _issuer, _type, _data,  _signature);
+    ownedDocumentList[_owner].push(totalDocument);
+  }
 
-  // function getOwnedDocumentList(address _account) public view returns (uint256[] memory) {
-  //   return ownedDocumentList[_account];
-  // }
+  function authorizeDocument(
+    address _requester,
+    address _owner,
+    string memory _documentDataList) public {
 
-  // function getAuthorizedDocumentList(address _account) public view returns (uint256[] memory) {
-  //   return authorizedDocumentList[_account];
-  // }
+    totalAuthorizedDocument++;
+    authorizedDocumentData[totalAuthorizedDocument] = AuthorizedDocumentData(totalAuthorizedDocument, _owner, _documentDataList);
+    authorizedDocumentList[_requester].push(totalAuthorizedDocument);
+  }
 
-  // function createDocumentNew(address _owner, string memory _type, string memory _data) public {
-  //   createDocument(_owner, _data);
+  function getOwnedDocumentList(address _account) public view returns (uint256[] memory) {
+    return ownedDocumentList[_account];
+  }
 
-  //   //assume totalDocument is incremented in createDocument() method
-  //   uint newId = totalDocument;
-  //   DocumentData memory newDocument = DocumentData(totalDocument, _type, _data, "signature");
-  //   ownedDocumentData[newId] = newDocument;
+  function getAuthorizedDocumentList(address _account) public view returns (uint256[] memory) {
+    return authorizedDocumentList[_account];
+  }
 
-  //   //insert mapping of document type to ids
-  //   // documentIdsOfType[_type].push(newId);
-  // }
+  // // function createDocumentNew(address _owner, string memory _type, string memory _data) public {
+  // //   createDocument(_owner, _data);
+
+  // //   //assume totalDocument is incremented in createDocument() method
+  // //   uint newId = totalDocument;
+  // //   DocumentData memory newDocument = DocumentData(totalDocument, _type, _data, "signature");
+  // //   ownedDocumentData[newId] = newDocument;
+
+  // //   //insert mapping of document type to ids
+  // //   // documentIdsOfType[_type].push(newId);
+  // // }
 
   // // function getDocumentByType(string memory _type) public view returns (uint256[] memory) {
   // //   return documentIdsOfType[_type];
